f180506c1d6649c018bb7eb003c2ed3d93a48ca4
tashkenna <natashakenna@gmail.com>
2023-07-20 13:46:00 +0100

    topics made dynamic

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,32 +4,37 @@ import { useEffect, useState } from "react";
 
 export const Categories = () => {
 
-  useEffect(()=> {
-    getTopics().then(data => {
-     
+  const [topics, setTopics] = useState([]);
+  const [error, setError] = useState(false)
 
+  useEffect(()=> {
+    getTopics().then(({topics}) => {
+  
+      const topicSlugs = topics.map((topic) => topic.slug)
+      setTopics(topicSlugs)
+     
       })
     .catch(err => {
-
+      setError(true)
     })
   }, [] )
 
 
+  console.log(topics)
+
   return (
     <div className="topic-buttons">
-
+    {error && <p>error retrieving topics</p>}
       <Link to="/articles" className="topic-button">
         all
       </Link>
-      <Link to="/articles/coding" className="topic-button">
-        coding
+
+      {topics.map(topic => 
+        <Link to={`/articles/${topic}`} className="topic-button">
+        {topic}
       </Link>
-      <Link to="/articles/cooking" className="topic-button">
-        cooking
-      </Link>
-      <Link to="/articles/football" className="topic-button">
-        football
-      </Link>
+      )}
+    
     </div>
   );
 };
