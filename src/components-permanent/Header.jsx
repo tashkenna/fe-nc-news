c987c5e54f149970d08caac337ca8884ce115996
tashkenna <natashakenna@gmail.com>
2023-07-21 16:20:58 +0100

    logo

--- src/components-permanent/Header.jsx
+++ src/components-permanent/Header.jsx
@@ -2,7 +2,7 @@
 export const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={"../NClogo.png"} alt="Northcoders Logo"/>
+            <img className="logo" src={"src/NClogo.png"} alt="Northcoders Logo"/>
             <h1 className="nav-text">NC News</h1>
         </div>
     )
