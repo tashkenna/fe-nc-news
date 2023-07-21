66456623a3f3f72bdc32c6d59a6a16119b3f2baf
tashkenna <natashakenna@gmail.com>
2023-07-21 16:14:10 +0100

    minor css changed made

--- src/components-permanent/Header.jsx
+++ src/components-permanent/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <div className="header">
-            <img className="logo" src={"src/NClogo.png"} alt="Northcoders Logo"/>
+            <img className="logo" src={"../src/NClogo.png"} alt="Northcoders Logo"/>
             <h1 className="nav-text">NC News</h1>
         </div>
     )
