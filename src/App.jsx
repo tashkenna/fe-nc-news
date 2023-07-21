af0c6e19224b532e5778eeaea7d574cd61276b50
tashkenna <natashakenna@gmail.com>
2023-07-21 14:06:20 +0100

    error handling complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Articles } from './components-pages/Articles'
 import { Article } from './components-pages/Article'
 import { Login } from './components-pages/Login'
 import { Toaster } from 'react-hot-toast';
+import { Error } from './components-pages/Error'
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
     <Route path="/articles" element={<Articles />} />
     <Route path='/articles/:id' element={<Article />} />
     <Route path='/login' element={<Login />} />
+    <Route path="*" element={<Error
+    errorStatus={404}
+    errorMessage={"not found: page does not exist"} />} />
   </Routes>
   </div>
 }
--- src/components-pages/Error.jsx
+++ src/components-pages/Error.jsx
@@ -1,5 +1,8 @@
-export const Error = () => {
-    return (<div> 
-    <p>Page Not Found</p>
+export const Error = (params) => {
+    const {errorStatus, errorMessage} = params
+    return (<div className="error-page"> 
+    <h1>{errorStatus} error</h1>
+    <h1>{errorMessage}</h1>
+    <p>please try again</p>
     </div>)
 }
